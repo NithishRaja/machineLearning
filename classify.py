eb44aed2213739a5cc566be72b7a520ac31d7968
NithishRaja <nithish98raja@gmail.com>
2019-04-11 16:43:27 +0530

    add code to get probability of each class

--- classify.py
+++ classify.py
@@ -33,6 +33,43 @@ class Classify:
             parse.main()
         # Setting number of data available
         self.dataSize = config["dataSize"]
+        # Reading frequency data file
+        file = ReadFile(self.frequencyFileName)
+        self.frequencyData = json.loads(file.read())
+        # Initializing object to hold probability for positive and negative statements
+        self.prob = {
+            "pos": self.frequencyData["posFrequency"]/self.trainingSetSize,
+            "neg": self.frequencyData["negFrequency"]/self.trainingSetSize
+        }
 
     # Function to classify data
-    # def main(self):
+    def main(self):
+        # Initializing variable to hold fileDescriptor
+        file = None
+        try:
+            # Opening file
+            file = open(self.dataFileName, "r", encoding="utf8")
+        except FileNotFoundError:
+            # Printing error
+            print("file not found at "+self.dataFileName)
+        else:
+            # Initializing counter for getting number of data instances
+            counter = 0
+            # iterating over each line in data
+            for line in file:
+                # Updating counter
+                counter = counter + 1
+                # Run code only for validation set i.e., last 20 % of data
+                if counter>self.trainingSetSize:
+                    # Splitting line into separate words
+                    words = re.split(" ", line)
+                    # Iterating over each word in current line
+                    for i in range(3, len(words)):
+                        # Calling function to get conditional probability of current word
+                        self.getConditionalFreqency(word[i])
+            # Closing file
+            file.close()
+            # Calling function to persist frequency data
+
+    # Function to get conditional frequency of given word
+    # def getConditionalFreqency(self, word):
