2484f5105526122db53061ad91438e0a28c63d98
NithishRaja <nithish98raja@gmail.com>
2019-05-16 11:43:34 +0530

    change class labels and add code to read data

--- classify.py
+++ classify.py
@@ -21,18 +21,12 @@ class Classify:
         self.fileName = config["fileName"]
         # Setting no of classes
         self.noOfClasses = config["noOfClasses"]
-        # Setting number of data available
-        self.dataSize = config["dataSize"]
         # Setting no of features in each data
         self.noOfFeatures = config["noOfFeatures"]
         # Initializing data object (Data is created asuming equal number of training data is available for all classes)
         self.data = []
         # Initializing counters for classes
         self.counter = numpy.zeros((self.noOfClasses))
-        # Setting learning rate
-        self.learningRate = config["learningRate"]
-        # Initializing weight vector
-        self.weightVector = numpy.arange(self.noOfFeatures+1, dtype=float)
         # Calling function to get data
         self.getData()
 
@@ -44,10 +38,10 @@ class Classify:
         # Iterating over data to get no of instances for each class
         for row in reader:
             # Updating counter
-            self.counter[int(row[-1])]=self.counter[int(row[-1])]+1
+            self.counter[int(row[0])]=self.counter[int(row[0])]+1
         # Setting Array size for data
         for i in range(self.noOfClasses):
-            self.data.append(numpy.zeros((self.noOfFeatures, int(self.counter[i])), dtype=float))
+            self.data.append(numpy.zeros((int(self.counter[i]), self.noOfFeatures), dtype=float))
         # Resetting file to top
         file.seek(0)
         # Resetting counter
@@ -57,89 +51,15 @@ class Classify:
             # Iterating over features
             for i in range(self.noOfFeatures):
                 # Storing features in data object
-                self.data[int(row[-1])][i][int(self.counter[int(row[-1])])] = row[i+1]
+                self.data[int(row[0])][int(self.counter[int(row[0])])][i] = row[i+1]
             # Updating counter
-            self.counter[int(row[-1])]=self.counter[int(row[-1])]+1
+            self.counter[int(row[0])]=self.counter[int(row[0])]+1
 
     # Function to classify data
-    def main(self):
-        # Counter for misclassified data
-        misclassifiedData = 0
-        # Iterating over each class
-        for i in range(self.noOfClasses):
-            # Iterating over data in each class
-            for j in range(int(self.dataSize/self.noOfClasses)):
-                value = self.weightVector[self.noOfFeatures]
-                # Iterating over each feature
-                for k in range(self.noOfFeatures):
-                    value = value + self.data[i][k][j]*self.weightVector[k]
-                # Checking if data is misclassified
-                if value*(i-0.5)<0:
-                    # Update counter for misclassified data
-                    misclassifiedData = misclassifiedData+1
-                    # Updating weight vector
-                    self.weightVector[self.noOfFeatures] = self.weightVector[self.noOfFeatures]+self.learningRate*2*(i-0.5)
-                    for k in range(self.noOfFeatures):
-                        self.weightVector[k] = self.weightVector[k]+self.learningRate*2*(i-0.5)*self.data[i][k][j]
-        # Returning no of misclassified data
-        return misclassifiedData
-
-    # Initializing function to plot data
-    def plotPoints(self, plotName, title):
-        # Plotting data points
-        # Setting class properties
-        marker = ["ro", "go"]
-        # Setting title
-        pyplot.title(title)
-        # Iterating over each class
-        for i in range(self.noOfClasses):
-            # Iterating over data in each class
-            for j in range(int(self.dataSize/self.noOfClasses)):
-                # If first data in this class, add label
-                if j==0:
-                    pyplot.plot(self.data[i][0][j], self.data[i][1][j], marker[i], markerfacecolor='none', label="class "+str(i))
-                else:
-                    pyplot.plot(self.data[i][0][j], self.data[i][1][j], marker[i], markerfacecolor='none')
-        # Plotting hyperplane to separate classes
-        x = numpy.linspace(-3, 3, 3)
-        y = -(self.weightVector[0]*x+self.weightVector[2])/self.weightVector[1]
-        pyplot.plot(x, y)
-        # Setting label
-        pyplot.legend()
-        # Saving plot as an image file
-        pyplot.savefig(plotName)
-        # Closing plot
-        pyplot.close()
+    # def main(self):
 
     # Initializing function to calculate error %
-    def calculateError(self):
-        # Initializing counter for misclassified data
-        misclassifiedData = 0
-        # Iterating over each class
-        for i in range(self.noOfClasses):
-            # Iterating over data in each class
-            for j in range(int(self.dataSize/self.noOfClasses)):
-                value = self.weightVector[self.noOfFeatures]
-                # Iterating over each feature
-                for k in range(self.noOfFeatures):
-                    value = value + self.data[i][k][j]*self.weightVector[k]
-                # Checking if data is misclassified
-                if (value<0 and i==1) or (value>0 and i==0):
-                    misclassifiedData = misclassifiedData+1
-        # Printing number of misclassified data and error %
-        print("No of misclassified data: ", misclassifiedData)
-        print("Error %: ", (misclassifiedData/self.dataSize)*100)
+    # def calculateError(self):
 
     # Function to classify new point
-    def classifyPoint(self, point):
-        distance = 0.0
-        # Calculating product between w vector and point
-        for i in range(self.noOfFeatures):
-            distance = distance + point[i]*self.weightVector[i]
-        # Adding bias
-        distance = distance + self.weightVector[self.noOfFeatures]
-        # Checking if data belongs to class 0 or class 1
-        if distance<0:
-            print("Class 0")
-        else:
-            print("Class 1")
+    # def classifyPoint(self, point):
--- index.py
+++ index.py
@@ -4,31 +4,7 @@
 #
 
 # Dependencies
-import os
 from classify import Classify
-
-# Checking if plot directory exists,
-# If it doesn't exist create it
-if not os.path.exists("plot/"):
-    os.mkdir("plot/")
 
 # Creating object
 classify = Classify()
-# Plotting initial hyperplane
-classify.plotPoints("plot/initial_plot.png", "Initial plot")
-# Initializing counter for epoch
-epochCounter = 1
-# Initializing maximum number of epoch
-epochLimit = 500
-# Initializing counter for misclassified data (Atleast one data is misclassified initially)
-misclassifiedData = 1
-# Iterating untill all data is correctly classified or maximum number of epoch is reached
-while misclassifiedData!=0 and epochCounter<epochLimit:
-    misclassifiedData = classify.main()
-    # Update epoch counter
-    epochCounter = epochCounter+1
-classify.plotPoints("plot/final_plot.png", "Final plot")
-# Printing error %
-classify.calculateError()
-# Printing no of epoch
-print("Epoch: ", epochCounter-1)
